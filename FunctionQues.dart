a3c1552a939fb092a3181f09aac3175f18b04423
Jamilur Rahman <rahmanjamil2709@gmail.com>
2026-02-27 21:48:40 +0600

    Update and rename FunctionQues.txt to FunctionQues.dart

--- FunctionQues.dart
+++ FunctionQues.dart
@@ -191,4 +191,5 @@ void main() {
   print("Default area: ${calculateArea()}");          
   print("Area (5 x 3): ${calculateArea(length: 5, width: 3)}");
   print("Area (length only): ${calculateArea(length: 4)}");
+
 }
